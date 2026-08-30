ae0789376a1def2121cdd7f83f5135ecabe766bd
Maz0224 <amaz2.201224@gmail.com>
2026-08-30 13:33:55 -0700

    Add support for multiple image stashes in viewer
    Refactor image viewer to support multiple stashes.

--- script.js
+++ script.js
@@ -1,70 +1,98 @@
-const images = [
-    "1.png",
-    "2.png",
-    "3.png",
-    "4.png",
-    "5.png",
-    "6.png",
-    "7.png"
-];
+const stashImages = {
+stash1: [
+"1.png",
+"2.png",
+"3.png",
+"4.png",
+"5.png",
+"6.png",
+"7.png"
+],
 
+```
+stash2: [
+    "stash2.1.png",
+    "stash2.2.png",
+    "stash2.3.png",
+    "stash2.4.png",
+    "stash2.5.png",
+    "stash2.6.png",
+    "stash2.7.png"
+]
+```
+
+};
+
+let currentStash = [];
 let currentImage = 0;
 
-function openViewer(image) {
-    currentImage = images.indexOf(image);
+function openViewer(image, stash = "stash1") {
+currentStash = stashImages[stash];
+currentImage = currentStash.indexOf(image);
 
-    if (currentImage === -1) {
-        currentImage = 0;
-    }
+```
+if (currentImage === -1) {
+    currentImage = 0;
+}
 
-    updateViewer();
+updateViewer();
 
-    document.getElementById("viewer").style.display = "flex";
+document.getElementById("viewer").style.display = "flex";
+```
+
 }
 
 function updateViewer() {
-    document.getElementById("viewerImage").src = images[currentImage];
+document.getElementById("viewerImage").src = currentStash[currentImage];
 }
 
 function nextImage() {
-    currentImage++;
+currentImage++;
 
-    if (currentImage >= images.length) {
-        currentImage = 0;
-    }
+```
+if (currentImage >= currentStash.length) {
+    currentImage = 0;
+}
 
-    updateViewer();
+updateViewer();
+```
+
 }
 
 function previousImage() {
-    currentImage--;
+currentImage--;
 
-    if (currentImage < 0) {
-        currentImage = images.length - 1;
-    }
+```
+if (currentImage < 0) {
+    currentImage = currentStash.length - 1;
+}
 
-    updateViewer();
+updateViewer();
+```
+
 }
 
 function closeViewer() {
-    document.getElementById("viewer").style.display = "none";
+document.getElementById("viewer").style.display = "none";
 }
 
 document.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
-        closeViewer();
-    }
+if (document.getElementById("viewer").style.display !== "flex") {
+return;
+}
 
-    if (event.key === "ArrowRight") {
-        nextImage();
-    }
+```
+if (event.key === "Escape") {
+    closeViewer();
+}
 
-    if (event.key === "ArrowLeft") {
-        previousImage();
-    }
-});
+if (event.key === "ArrowRight") {
+    nextImage();
+}
 
-document.addEventListener("mousemove", function(event) {
-document.body.style.setProperty("--mouse-x", event.clientX + "px");
-document.body.style.setProperty("--mouse-y", event.clientY + "px");
+if (event.key === "ArrowLeft") {
+    previousImage();
+}
+```
+
 });
